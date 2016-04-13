bb97e0ea740a59b6156dad5cadf11c0ab54a286f
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-13 13:53:47 -0600

    edited to the max

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
 		<title>about-me-Elliot-EE</title>
 	</head>
 	<body>
-		<h1>about me</h1>
-		Hi all my name is <strong>Elliot</strong> aka <em>EE</em>
+		<h1>My Name is Elliot Murrey aka <em>EE</em></h1>
+			<p><img src="images/IMAG0907.jpg" alt="pictur of me" width="200" height="280"></p>
+		<h2>History</h2>
+			<p>I was born In Sacramento California on December, 13,1989 but have been in Albuquerque for all of my life, and love the heck out of it.</p>
+		<h2> Hobbies</h2>
+		<p> I love Halo a freaking lot </p>
+		<p> <img src="images/url.jpg" alt="halo picture" width="199" Height="112"> </p>
 	</body>
 </html>
