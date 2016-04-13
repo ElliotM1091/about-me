220ca7e8462ad90741b3fafd8e6e56821f61861f
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-13 12:38:45 -0600

    deleted .gitignor.php and added reg gitignor

--- index.php
+++ index.php
@@ -4,4 +4,8 @@
 		<meta charset="utf-8"/>
 		<title>about-me-Elliot-EE</title>
 	</head>
+	<body>
+		<h1>about me</h1>
+		Hi all my name is <strong>Elliot</strong> aka <em>EE</em>
+	</body>
 </html>
