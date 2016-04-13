e92040e740ed2cd67f687c5847ffeac464746feb
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-13 14:15:59 -0600

    edited to the max

--- index.php
+++ index.php
@@ -8,9 +8,13 @@
 		<h1>My Name is Elliot Murrey aka <em>EE</em></h1>
 			<p><img src="images/IMAG0907.jpg" alt="pictur of me" width="200" height="280"></p>
 		<h2>History</h2>
-			<p>I was born In Sacramento California on December, 13,1989 but have been in Albuquerque for all of my life, and love the heck out of it.</p>
+			<p>I was born In Sacramento California on December, 13,1989 but have been in Albuquerque for all of my life, and love the heck out of it. I've been gaming since I was a kid I have 2 kids and a hot wife</p>
 		<h2> Hobbies</h2>
-		<p> I love Halo a freaking lot </p>
+		<p> I love <strong>Halo</strong> a freaking lot. If you ever want to get beat at Halo just hit me up. if you want me to destroy you in any other first person shooter I'll be happy to oblige !!  </p>
 		<p> <img src="images/url.jpg" alt="halo picture" width="199" Height="112"> </p>
-	</body>
+		<h5>I also like</h5>
+			<p> <ul><li> Call of duty(all of them)</li><li>battlefield</li><li>and so much more</li></p>
+		<h2>Flash is 100% Awesome!!</h2>
+		<p><img src="images/imgres.jpg" alt="flash picture"></p>
+</body>
 </html>
