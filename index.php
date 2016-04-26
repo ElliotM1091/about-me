3d0e05f10cb5e2b9c11cf69f6b686d9450404c1c
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-26 09:41:39 -0600

    added stuff you know

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>about-me-Elliot-EE</title>
-		<link rel="styles" href="style.css" type="text/css"
+		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
 	<body>
 		<h1>My Name is Elliot Murrey aka <em>EE</em></h1>
