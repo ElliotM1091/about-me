13ffb73e751c9f59c8570edd77230b1d89887909
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-05 09:44:41 -0600

    doing bootstrap funk ya

--- index.php
+++ index.php
@@ -2,19 +2,48 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<!--sets IE rendering to IE-EDGE-->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+				integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<!--my customer Css-->
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<script   src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 		<title>about-me-Elliot-EE</title>
-		<link rel="stylesheet" type="text/css" href="style.css" />
 	</head>
 	<body>
+		info recieved from mozillla developer network <a href="https://developer.mozilla.org">MDN</a>
 		<h1>My Name is Elliot Murrey aka <em>EE</em></h1>
-			<p><img src="images/IMAG0907.jpg" alt="pictur of me" width="200" height="280"></p>
+		<p><img class="pull-left" src="images/IMAG0907.jpg" alt="pictur of me" width="200" height="280"></p>
 		<h2>History</h2>
-			<p>I was born In Sacramento California on December, 13,1989 but have been in Albuquerque for all of my life, and love the heck out of it. I've been gaming since I was a kid I have 2 kids and a hot wife</p>
+		<p>I was born In Sacramento California on December, 13,1989 but have been in Albuquerque for all of my life, and
+			love the heck out of it. I've been gaming since I was a kid I have 2 kids and a hot wife</p>
 		<h2> Hobbies</h2>
-		<p> I love <strong>Halo</strong> a freaking lot. If you ever want to get beat at Halo just hit me up. if you want me to destroy you in any other first person shooter I'll be happy to oblige !!  </p>
-		<p> <img src="images/url.jpg" alt="halo picture" width="199" Height="112"> </p>
+		<p> I love <strong>Halo</strong> a freaking lot. If you ever want to get beat at Halo just hit me up. if you want
+			me to destroy you in any other first person shooter I'll be happy to oblige !! </p>
+		<p><img src="images/url.jpg" alt="halo picture" width="199" Height="112"></p>
 		<h5>I also like</h5>
-			<p> <ul><li> Call of duty(all of them)</li><li>battlefield</li><li>and so much more</li></p>
+		<p>
+		<ul>
+			<li> Call of duty(all of them)</li>
+			<li>battlefield</li>
+			<li>and so much more</li>
+		</p>
 		<h2>Flash is 100% Awesome!!</h2>
 		<p><img src="images/imgres.jpg" alt="flash picture"></p>
 		<h2>Multiplication chart</h2>
@@ -105,5 +134,5 @@
 
 			</tr>
 		</table>
-</body>
+	</body>
 </html>
